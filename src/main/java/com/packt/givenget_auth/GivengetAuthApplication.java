11e6bc8cce99fe4d18b22b0f95c042ea857e4306
Aaronarphaxad <aaronarphaxad@gmail.com>
2025-04-14 17:42:09 -0700

    initial commit

---
+++ src/main/java/com/packt/givenget_auth/GivengetAuthApplication.java
@@ -0,0 +1,13 @@
+package com.packt.givenget_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GivengetAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GivengetAuthApplication.class, args);
+	}
+
+}
